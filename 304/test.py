e75cf7eac5c45fbdfd7032aa4d79072c4d2695eb
Bryce Wilson <bryce.wilson@alumni.ubc.ca>
2021-03-20 21:13:40 -0700

    Test commit

--- 304/test.py
+++ 304/test.py
@@ -5,7 +5,7 @@ print("Content-type: text/plain\n")  # DO NOT REMOVE, MUST BE DONE FIRST
 
 print("Hello, World!\n")
 print("Welcome! Your CWL username is: " + os.environ["REMOTE_USER"] + "\n")
-print("Here are the environment variables which are avalible in this script:")
+print("Here are the environment variables which are available in this script:")
 
 for key, value in os.environ.items():
     print(f"    {key}: {value}")
