ae5cbdb353ad3e3208ad54462c402c22b72aa3c6
Bryce Wilson <bryce@brycemw.ca>
2021-03-20 21:03:57 -0700

    Add information on how to configure deployment.

---
+++ 304/test.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+import os
+print("Content-type: text/plain\n")  # DO NOT REMOVE, MUST BE DONE FIRST
+
+
+print("Hello, World!\n")
+print("Welcome! Your CWL username is: " + os.environ["REMOTE_USER"] + "\n")
+print("Here are the environment variables which are avalible in this script:")
+
+for key, value in os.environ.items():
+    print(f"    {key}: {value}")
