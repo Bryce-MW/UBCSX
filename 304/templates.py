02bfaea23bc951256b174542a79f9b1a5a45cef7
Bryce Wilson <brycemw@students.cs.ubc.ca>
2021-03-27 16:46:27 -0700

    Initial values of search

--- 304/templates.py
+++ 304/templates.py
@@ -10,3 +10,9 @@ position = """            <span><a href="symbols.py?{symbol_url}">{symbol}</a></
             <span>{percent:.2f}%</span>
             <span>{count}</span>
             """
+search_result = """            <span><a href="symbols.py?{symbol_url}">{symbol}</a></span>
+            <span><a href="symbols.py?{symbol_url}">{name}</a></span>
+            <span>${last:.2f}</span>
+            <span>${bid:.2f}</span>
+            <span>${ask:.2f}</span>
+            """
