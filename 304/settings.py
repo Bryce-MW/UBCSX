21533a586a3799231cba848bf84567de19c4efe6
michyliu <michyliu@students.cs.ubc.ca>
2021-03-27 19:29:05 -0700

    Settings doesn't need account name, orders needs account name

--- 304/settings.py
+++ 304/settings.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-from ubcsx import cursor, post, redirect, script_html, urlencode, user, unescape, params, escape
+from ubcsx import cursor, post, redirect, script_html, user, unescape, params
 
 cursor.execute("SELECT owner_name FROM owners WHERE owner=%s", (user,))
 name = cursor.fetchone()["owner_name"]
@@ -7,14 +7,11 @@ name = cursor.fetchone()["owner_name"]
 if not post:
     print(script_html.format(**globals()))
 else:
-    if "account" in params:
-        account_name = unescape(params["account"][0])
-        try:
-            new_name = post["name"][0]
-        except (KeyError, IndexError):
-            # Something was not specified for some reason. Consider adding warning
-            print(script_html.format(**globals()))
-            exit()
-        cursor.execute("UPDATE owners SET owner_name=%s WHERE owner=%s", (new_name, user))
-        encoded = urlencode({"account": account_name})
-        redirect(f"main.py?{encoded}")
+    try:
+        new_name = post["name"][0]
+    except (KeyError, IndexError):
+        # Something was not specified for some reason. Consider adding warning
+        print(script_html.format(**globals()))
+        exit()
+    cursor.execute("UPDATE owners SET owner_name=%s WHERE owner=%s", (new_name, user))
+    redirect("main.py")
