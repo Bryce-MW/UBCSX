b2299c23e9afec6b0169a4d1f2a2f404b3b42ed8
Bryce Wilson <brycemw@students.cs.ubc.ca>
2021-03-27 19:35:28 -0700

    Finish search

--- 304/search.py
+++ 304/search.py
@@ -5,7 +5,7 @@ from ubcsx import names, symbols, script_html, params, current_page, urlencode, 
 search_results = ""
 rows = 0
 
-query = params["query"][0] if "query" in params else ""
+query = params["q"][0] if "q" in params else ""
 query_url = urlencode({"query":query})
 
 if query:
@@ -32,17 +32,16 @@ if query:
         search_results += search_result.format(symbol_url=urlencode({"symbol": row['symbol']}), symbol=escape(row["symbol"]), name=escape(row["name"]), last=format_ba(row["last"]), bid=format_ba(row["bid"]), ask=format_ba(row["ask"]))
 
     cursor.execute("""
-            SELECT etfs.symbol AS symbol, accounts.account_name AS name, SUM((stocks.last_price + accounts.balance) / (owns.units / (SELECT SUM(units) FROM owns AS o2 where o2.symbol=owns.symbol))) AS last
-        FROM shares INNER JOIN stocks ON shares.symbol=stocks.symbol INNER JOIN accounts ON accounts.id=shares.owned_by_account_id RIGHT JOIN etfs ON accounts.id=etfs.controls_account_id LEFT JOIN owns ON owns.symbol=etfs.symbol
+        SELECT etfs.symbol AS symbol, accounts.account_name AS name, (IFNULL(SUM(stocks.last_price), 0) + accounts.balance) / IFNULL((SELECT SUM(units) FROM owns AS o2 where o2.symbol=etfs.symbol), 1) AS last
+        FROM etfs INNER JOIN accounts ON accounts.id=etfs.controls_account_id LEFT JOIN shares ON accounts.id=shares.owned_by_account_id LEFT JOIN stocks ON shares.symbol=stocks.symbol
         GROUP BY etfs.symbol, accounts.account_name
         HAVING etfs.symbol LIKE CONCAT('%', %(q)s, '%') OR accounts.account_name LIKE CONCAT('%', %(q)s, '%')
-        ORDER BY etfs.symbol LIKE CONCAT(%(q)s, '%'), accounts.account_name LIKE CONCAT(%(q)s, '%'), etfs.symbol LIKE CONCAT('%', %(q)s, '%'), accounts.account_name LIKE CONCAT('%', %(q)s, '%'), etfs.symbol
+        ORDER BY etfs.symbol LIKE CONCAT(%(q)s, '%') DESC, accounts.account_name LIKE CONCAT(%(q)s, '%') DESC, etfs.symbol LIKE CONCAT('%', %(q)s, '%') DESC, accounts.account_name LIKE CONCAT('%', %(q)s, '%') DESC, etfs.symbol
         """, {"q":query})
 
     for row in cursor:
         rows += 1
         search_results += search_result.format(symbol_url=urlencode({"symbol": row['symbol']}), symbol=escape(row["symbol"]), name=escape(row["name"]), last=format_ba(row["last"]), bid="", ask="")
-
 
 
 print(script_html.format(**globals()))
