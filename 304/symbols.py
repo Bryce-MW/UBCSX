6809416695cabe89c7abebaf3da157cb108d5f63
Bryce Wilson <brycemw@students.cs.ubc.ca>
2021-03-27 21:49:53 -0700

    Allow the symbols page to open

--- 304/symbols.py
+++ 304/symbols.py
@@ -1,4 +1,7 @@
 #! /usr/bin/python3
-from ubcsx import script_html, names, symbols, current_page
+from ubcsx import script_html, names, symbols, current_page, params
+
+symbol = params["symbol"] if "symbol" in params else ""
+
 
 print(script_html.format(**globals()))
