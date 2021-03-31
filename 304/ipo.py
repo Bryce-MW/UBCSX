986ead9b0eda00b7053e6009663cb4d0e51a76b3
zhangwil <williamzy314@gmail.com>
2021-03-31 14:19:45 -0700

    ipo.py bug fix
    TODO: fix redirect broken

--- 304/ipo.py
+++ 304/ipo.py
@@ -4,8 +4,6 @@ from ubcsx import cursor, post, redirect, script_html, urlencode, user, current_
 if not post:
     print(script_html.format(**globals()))
 else:
-    print(post)
-    print(user)
     print(script_html.format(**globals()))
 
     if post["type"][0] == "Company":
@@ -14,13 +12,17 @@ else:
             symbol = cursor.fetchone()
             cursor.execute("SELECT owner_name FROM owners WHERE owner = %s", (user,))
             owner_name = cursor.fetchone()["owner_name"]
+            cursor.execute("SELECT ceo FROM ceos WHERE ceo = %s", (owner_name,))
+            ceo = cursor.fetchone()
 
             if symbol is not None:
                 redirect("main.py", "A stock with that symbol already exists.")
                 exit()
 
-            cursor.execute("INSERT INTO ceos (ceo) VALUES %s", (owner_name,))
-            cursor.execute("INSERT INTO stocks (name, symbol, ceo) VALUES (%(name)s, %(symbol)s, %(ceo)s)", {"name": post["ipo-name"], "symbol": post["ipo-symbol"], "ceo": owner_name})
+            if ceo is None:
+                cursor.execute("INSERT INTO ceos (ceo) VALUES (%s)", (owner_name,))
+
+            cursor.execute("INSERT INTO stocks (name, symbol, ceo) VALUES (%(name)s, %(symbol)s, %(ceo)s)", {"name": post["ipo-name"][0], "symbol": post["ipo-symbol"][0], "ceo": owner_name})
         except (KeyError, IndexError):
             # Something was not specified for some reason. Consider adding warning
             print(script_html.format(**globals()))
@@ -29,15 +31,22 @@ else:
         try:
             cursor.execute("SELECT symbol FROM etfs WHERE symbol = %s", (post["ipo-symbol"][0],))
             symbol = cursor.fetchone()
+            cursor.execute("SELECT id FROM accounts WHERE owner = %s", (user,))
+            account_id = cursor.fetchone()["id"]
+            cursor.execute("SELECT * FROM etfs WHERE controls_account_id = %s", (account_id,))
+            etf = cursor.fetchone()
 
             if symbol is not None:
                 redirect("main.py", "An ETF with that symbol already exists.")
                 exit()
+            elif etf is not None:
+                redirect("main.py", "You already have an ETF.")
+                exit()
 
-            cursor.execute("INSERT INTO etfs (symbol, controls_account_id) VALUES (%(symbol)s, %(account_id)s)", {"symbol": post["ipo-symbol"], "account_id": user})
+            cursor.execute("INSERT INTO etfs (symbol, controls_account_id) VALUES (%(symbol)s, %(account_id)s)", {"symbol": post["ipo-symbol"][0], "account_id": account_id})
         except (KeyError, IndexError):
             # Something was not specified for some reason. Consider adding warning
             print(script_html.format(**globals()))
             exit()
 
-    redirect(f"main.py", "IPO added successfully.")
+    redirect("main.py", "IPO added successfully.")
