b75ced4f321ce10061b0eede454c8eec85862ee2
Bryce Wilson <brycemw@students.cs.ubc.ca>
2021-03-31 23:18:10 -0700

    Fix more bugs

--- 304/main.py
+++ 304/main.py
@@ -25,7 +25,7 @@ if account_name not in account_list and not all_selected:
     redirect("main.py?" + urlencode({"account":account_list[0]}))
     exit()
 
-rows = 1
+rows = 0
 positions = ""
 
 if "account" in params:
@@ -49,7 +49,7 @@ if "account" in params:
     cursor.execute("""
         SELECT a.symbol AS symbol, last, bid, ask, count, value, percent FROM
             (SELECT shares.symbol AS symbol, stocks.last_price AS last, COUNT(*) AS count, COUNT(*)*stocks.last_price AS value, COUNT(*)*stocks.last_price/%s AS percent FROM shares INNER JOIN stocks on shares.symbol=stocks.symbol INNER JOIN accounts on shares.owned_by_account_id=accounts.id WHERE owner=%s AND account_name=%s GROUP BY symbol) AS a
-        INNER JOIN
+        LEFT JOIN
             (SELECT bid, ask, bid_table.symbol FROM
                 (SELECT max(price) AS bid, symbol FROM `limit` INNER JOIN orders ON `limit`.order_id=orders.id WHERE orders.quantity>0 GROUP BY symbol) AS bid_table
             LEFT JOIN
@@ -62,7 +62,7 @@ if "account" in params:
             ON bid_table.symbol=ask_table.symbol) AS b
         ON a.symbol=b.symbol""", (market_value, user, account_name))
     for row in cursor:
-        rows += 0
+        rows += 1
         account = params["account"][0] if "account" in params else ""
         symbol_url = escape(urlencode({"symbol":row['symbol'], "account":account}))
         positions += position.format(symbol=escape(row['symbol']), symbol_url=escape(symbol_url), last=float(row['last']) / dollar, bid=format_ba(row['bid']), ask=format_ba(row['ask']), percent=row['percent'] * 100, count=row['count'], value=float(row['value']) / dollar)
